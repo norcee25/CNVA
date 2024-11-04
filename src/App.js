1aec3dd88531df41615fe3e8ad5508ef6b77f943
norcee25 <s22210076@student.unklab.ac.id>
2024-11-04 08:16:20 +0800

    app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import Header from './component/Header'
 import Home from './component/Home'
 import Footer from './component/Footer'
-
-
 
 const App = () => {
   const cnvaapp= "Hello (App)"
@@ -44,4 +15,3 @@ const App = () => {
 };
 
 export default App;
->>>>>>> 75d7a7d91fd7bdd106319a118df2cfbb777dd608
