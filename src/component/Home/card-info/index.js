fa813f69365b4f77d7c118f0a1b74d42f758ee14
carollangi <s22210434@student.unklab.ac.id>
2024-11-06 16:28:24 +0700

    adding props carol

--- src/component/Home/card-info/index.js
+++ src/component/Home/card-info/index.js
@@ -1,7 +1,18 @@
-const Evolution = () => {
-    return (
-        <h3>Evolution</h3>
-    )
-}
+import Evolution from './Evolution'
+import TE from './Type-Effect'
+import Logo from './Logo'
 
-export default Evolution;
+const ci = ({text,number}) => {
+    return (
+        <>
+        <h2>Card-Info</h2>
+        <p>
+            <Evolution text="adding props to Evolution" number={1}/>
+            <TE text="adding props to Type-Effect" number={2}/>
+            <Logo text="adding props to Logo Card-Info" number={3}/>
+        </p>
+        </>
+    );
+};
+
+export default ci;
